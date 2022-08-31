6c073eab1858a12a6c9e9ea943ecb4a8f16c7b0f
jfray107 <jacob.francis107@gmail.cok>
2022-08-31 20:39:37 +0100

    Readded .cfg files that were accidentally removed. Removed files relating to uhdf5 implementation as this is no longer used

--- UHDF5/uhdf5-kt-1.0-SNAPSHOT/uk/ac/shef/planar/alias/alias.kt
+++
@@ -1,7 +0,0 @@
-package uk.ac.shef.planar.alias
-
-typealias PlanarDataset = uk.ac.shef.planar.Dataset
-typealias PlanarDimensionDescription = uk.ac.shef.planar.DimensionDescription
-typealias PlanarDescription = uk.ac.shef.planar.Description
-typealias PlanarCoordinate = uk.ac.shef.planar.Coordinate
-
