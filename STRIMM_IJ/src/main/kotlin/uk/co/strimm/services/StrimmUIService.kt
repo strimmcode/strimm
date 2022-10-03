bcff2dd649fde2efd8e334f6dcd6c1c6a00c8a31
jfray107 <jacob.francis107@gmail.cok>
2022-10-03 17:37:43 +0100

    Code syntax improvements for STRIMMUIService

--- STRIMM_IJ/src/main/kotlin/uk/co/strimm/services/StrimmUIService.kt
+++ STRIMM_IJ/src/main/kotlin/uk/co/strimm/services/StrimmUIService.kt
@@ -40,23 +40,24 @@ class StrimmUIService : AbstractService(), ImageJService {
 
     var traceColourNonROI = 0
 
-    fun SetROI(label : String, x : Long, y : Long, w : Long, h : Long) : Int {
+    fun SetROI(label: String, x: Long, y: Long, w: Long, h: Long): Int {
         val camSizes = Pair(w, h)
         GUIMain.strimmUIService.cameraSizeList[label] = camSizes
         return 0
     }
+
     //cameraSizeList is a list of the full sizes for camera feeds
-    public var cameraSizeList = hashMapOf<String?, Pair<Long?,Long?>>()
+    var cameraSizeList = hashMapOf<String?, Pair<Long?, Long?>>()
 
     //cameraViewSizeList is a list of the view sizes for each camera, this is used when the user resizes a camera feed
-    public var cameraViewSizeList = hashMapOf<String?, ResizeValues>()
+    var cameraViewSizeList = hashMapOf<String?, ResizeValues>()
 
     /**
      * This method is called when the user resizes a camera feed to an ROI they have drawn. This only works for
      * rectangular ROIs
      * @param cameraFeedDisplay The image display corresponding to the camera feed
      */
-    fun resizeCameraFeedToROI(cameraFeedDisplay : ImageDisplay){
+    fun resizeCameraFeedToROI(cameraFeedDisplay: ImageDisplay) {
         GUIMain.loggerService.log(Level.INFO, "Resizing camera feed to ROI")
         val selectedROI = GUIMain.overlayService.getActiveOverlay(cameraFeedDisplay)
         when (selectedROI) {
@@ -66,7 +67,7 @@ class StrimmUIService : AbstractService(), ImageJService {
                 val w = selectedROI.getExtent(0)
                 val h = selectedROI.getExtent(1)
                 val cameraActor = GUIMain.actorService.getPertainingCameraActorFromDisplay(cameraFeedDisplay)
-                cameraActor!!.tell(TellCameraResize(x,y,w,h), ActorRef.noSender())
+                cameraActor!!.tell(TellCameraResize(x, y, w, h), ActorRef.noSender())
             }
             is EllipseOverlay -> JOptionPane.showMessageDialog(strimmFrame, ComponentTexts.AcquisitionDialogs.RESIZE_MUST_BE_RECT)
             else -> JOptionPane.showMessageDialog(strimmFrame, ComponentTexts.AcquisitionDialogs.COULD_NOT_FIND_ROI_FOR_RESIZE)
@@ -74,48 +75,42 @@ class StrimmUIService : AbstractService(), ImageJService {
 
     }
 
-    fun enableStartStopButton(){
+    fun enableStartStopButton() {
         GUIMain.expStartStopButton.isEnabled = true
     }
+
     /**
      * This method is called when the user expands an image feed to full size view
      * @param cameraFeedDisplay The image display corresponding to the camera feed
      */
-    fun expandCameraFeedToFullView(cameraFeedDisplay : ImageDisplay){
+    fun expandCameraFeedToFullView(cameraFeedDisplay: ImageDisplay) {
         GUIMain.loggerService.log(Level.INFO, "Expanding camera feed to full view")
         val cameraActor = GUIMain.actorService.getPertainingCameraActorFromDisplay(cameraFeedDisplay)
         cameraActor!!.tell(TellFullView(), ActorRef.noSender())
     }
 
-    fun redrawROIs(cameraWindowPlugin: CameraWindowPlugin, editable : Boolean){
-        //function never used
+    fun redrawROIs(cameraWindowPlugin: CameraWindowPlugin, editable: Boolean) {
+        //TODO function never used
         //get the display from the CameraWindow
         val display = (cameraWindowPlugin.cameraWindowController.display as ImageDisplay)
         println("Redrawing overlay")
         //create a RectangleOverlay and fill in details  text?
         val newROI = RectangleOverlay() // IJ structure
-        newROI.setOrigin(250.0,0)
-        newROI.setOrigin(700.0,1)
-        newROI.setExtent(100.0,0)
-        newROI.setExtent(100.0,1)
-        newROI.regionOfInterest.setOrigin(250.0,0)
-        newROI.regionOfInterest.setOrigin(700.0,1)
-        newROI.regionOfInterest.setExtent(100.0,0)
-        newROI.regionOfInterest.setExtent(100.0,1)
-        newROI.lineColor = ColorRGB(125,125,125)
-        newROI.fillColor = ColorRGB(255,255,255)
+        newROI.setOrigin(250.0, 0)
+        newROI.setOrigin(700.0, 1)
+        newROI.setExtent(100.0, 0)
+        newROI.setExtent(100.0, 1)
+        newROI.regionOfInterest.setOrigin(250.0, 0)
+        newROI.regionOfInterest.setOrigin(700.0, 1)
+        newROI.regionOfInterest.setExtent(100.0, 0)
+        newROI.regionOfInterest.setExtent(100.0, 1)
+        newROI.lineColor = ColorRGB(125, 125, 125)
+        newROI.fillColor = ColorRGB(255, 255, 255)
         newROI.lineStyle = Overlay.LineStyle.SOLID
         newROI.alpha = 100
 
         java.awt.EventQueue.invokeLater {
-            val overlays1 = GUIMain.overlayService.getOverlays(display)
-            println("1 ${overlays1.size}")
             GUIMain.overlayService.drawOverlay(newROI, display, ChannelCollection())
-        }
-
-        java.awt.EventQueue.invokeLater {
-            val overlays2 = GUIMain.overlayService.getOverlays(display)
-            println("2 ${overlays2.size}")
         }
     }
 }
